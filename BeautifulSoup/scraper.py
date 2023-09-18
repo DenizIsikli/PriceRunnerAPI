b6fd2df7c04a20f1fa76c9a711f2bfa19f31c2fd
Bobby <s215818@dtu.dk>
2023-09-18 22:48:28 +0200

    API works

--- BeautifulSoup/scraper.py
+++ BeautifulSoup/scraper.py
@@ -5,13 +5,15 @@ import requests
 from bs4 import BeautifulSoup
 from dataclasses import dataclass
 
+# Flask is running on http://127.0.0.1:5000/search/iphone
+
 
 @dataclass
 class Product:
-    name: str
-    info: str
-    price: int
-    link: str
+    name: str = None
+    info: str = None
+    price: int = None
+    link: str = None
 
 
 class PriceRunnerAPI:
@@ -21,45 +23,49 @@ class PriceRunnerAPI:
 
         self.url = None
 
+        self.products = []
+
+        self.name = None
+        self.info = None
+        self.price = None
+        self.link = None
+
     def search_product(self, product_name):
         self.url = 'https://www.pricerunner.dk/search?q=' + product_name.replace(" ", "+")
 
         try:
-            products = []
-
             response = requests.get(self.url)
 
             if response.status_code == 200:
                 soup = BeautifulSoup(response.text, 'html.parser')
-                product_div = soup.find('div', class_='mIkxpLfxgo pr-lt04mx')
+                product_div = soup.find('div', class_='mIkxpLfxgo pr-1dtdlzd')
 
                 if product_div:
                     for item_div in product_div.find_all('div', class_='pr-1k8dg1g'):
                         # Extract product information within each product's container
-                        name_product = item_div.find('h3', class_='pr-z2tzuf').text
-                        info = item_div.find('p', class_='pr-1qhwt5w').text
-                        price = item_div.find('span', class_='pr-yp1q6p').text
-                        link = item_div.find('a')['href']
+                        self.name = item_div.find('h3', class_='pr-z2tzuf').text
+                        self.info = item_div.find('p', class_='pr-1qhwt5w').text
+                        self.price = item_div.find('span', class_='pr-yp1q6p').text
+                        self.price = self.price.replace('\xa0', '')
+                        self.link = item_div.find('a')['href']
 
-                        product = Product(name=name_product, info=info, price=price, link=link)
-                        products.append(product)
+                        product_dict = {'name': self.name, 'info': self.info, 'price': self.price, 'link': self.link}
+                        self.products.append(Product(**product_dict))
+                        print(self.products)
 
-                    if soup.find('button', class_='pr-5cnc2s'):
-                        return
-
-            return products
+                if soup.find('button', class_='pr-5cnc2s'):
+                    return self.products
 
         except requests.exceptions.RequestException as e:
             print(f"Error: {e}")
+
+        return self.products
 
     def run(self):
         @self.app.route('/search/<product_name>')
         def search_route(product_name):
             products = self.search_product(product_name)
-            product_dicts = [{'name': product.name, 'info': product.info, 'price': product.price, 'link': product.link}
-                             for product in products]
 
-            print(product_dicts)
-            return jsonify(product_dicts)
+            return jsonify(products)
 
         self.app.run(debug=True)
