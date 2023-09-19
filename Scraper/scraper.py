fe884fab0fed9a57b3113a3e407b687e96c0bc71
Bobby <s215818@dtu.dk>
2023-09-19 23:55:54 +0200

    Fix

--- Scraper/scraper.py
+++ Scraper/scraper.py
@@ -1,10 +1,8 @@
 from flask import Flask, jsonify, request
-from flask_restful import Api, Resource, reqparse
-import random
+from flask_restful import Api
 import requests
 from bs4 import BeautifulSoup
 from dataclasses import dataclass
-from flask.blueprints import Blueprint
 
 
 # Flask is running on http://127.0.0.1:5000/search/iphone
