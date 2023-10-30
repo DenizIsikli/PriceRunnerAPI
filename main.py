e683f18f4bca63ef2d20be6b9783bdd2ae4417f3
Bobby <s215818@dtu.dk>
2023-10-31 00:00:03 +0100

    database is initialized in run()

--- main.py
+++ main.py
@@ -1,26 +1,12 @@
 from scraper import PriceRunnerAPI
 from database import Database
 
-
 def main():
     # Initialize the API
     api = PriceRunnerAPI()
 
-    # Initialize the database
-    db = Database(
-        database="mydatabase",
-        user="myuser",
-        password="mypassword",
-        host="localhost",
-        port="5432"
-    )
-
     # Start the API
     api.run()
-
-    # Don't forget to close the database connection
-    db.close()
-
 
 if __name__ == '__main__':
     main()
