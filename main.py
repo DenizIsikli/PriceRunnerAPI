c3e4b26fcbbb318b8ef0d5e1039f3bb00bc5652a
Bobby <s215818@dtu.dk>
2023-10-30 23:57:47 +0100

    database

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
-import Scraper.scraper as scraper
+from scraper import PriceRunnerAPI
+from database import Database
 
 
 def main():
-    api = scraper.PriceRunnerAPI()
+    # Initialize the API
+    api = PriceRunnerAPI()
+
+    # Initialize the database
+    db = Database(
+        database="mydatabase",
+        user="myuser",
+        password="mypassword",
+        host="localhost",
+        port="5432"
+    )
+
+    # Start the API
     api.run()
+
+    # Don't forget to close the database connection
+    db.close()
 
 
 if __name__ == '__main__':
