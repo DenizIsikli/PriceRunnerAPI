4bd4679657c77e5df6cb9171fdf84b4cd0cdce9d
Bobby <s215818@dtu.dk>
2023-11-19 14:59:47 +0100

    Fix

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 from scraper import PriceRunnerAPI
-from database import Database
+
 
 def main():
     # Initialize the API
     api = PriceRunnerAPI()
-
-    # Start the API
     api.run()
+
 
 if __name__ == '__main__':
     main()
