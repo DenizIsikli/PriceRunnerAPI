ea1daff9f647ad6fb203ab83c7392ed22de2e663
Bobby <s215818@dtu.dk>
2023-11-21 01:27:38 +0100

    Structure

--- install.py
+++ install.py
@@ -6,7 +6,6 @@ import venv
 def create_virtual_environment():
     venv.create("venv", with_pip=True)
 
-
 def install_dependencies():
     subprocess.run(["venv/bin/python", "-m", "pip", "install", "--upgrade", "-r", "requirements.txt"])
 
